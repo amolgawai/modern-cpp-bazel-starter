84597be8f36770544a21e874f8fe919888dfb858
Amol <5114143+amolgawai@users.noreply.github.com>
2023-05-03 20:46:26 +0000

    First cut bazel layout

--- cmake/Misc.cmake
+++
@@ -1,6 +0,0 @@
-# --------------------------------------------------------------------------------
-# Misc (no change needed).
-# --------------------------------------------------------------------------------
-
-# Ask CMake to output a compile_commands.json file for use with things like Vim YCM.
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
